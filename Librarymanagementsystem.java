2cc50889b738709d05d4735f2a6761cfabc6417a
priyansh065 <priyansh.24sece1030065@galgotiasuniversity.ac.in>
2025-05-25 22:56:59 +0530

    Add files via upload

---
+++ Librarymanagementsystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.librarymanagementsystem;
+
+/**
+ *
+ * @author spars
+ */
+public class Librarymanagementsystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
